82bb304ed3289878715a2a092d7472bcfa291be8
LucasDev <lucasmndes.dev@gmail.com>
2026-02-10 19:19:35 -0300

    implement the health route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Head, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @AllowAnonymous()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @AllowAnonymous()
+  @Get('health')
+  healthCheck() {
+    return {
+      status: HttpStatus.OK,
+      message: 'Service is healthy',
+    }
   }
 }
